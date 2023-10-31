cfd4ff6444b3259c181c538c96be9ef77720adb6
AliReza <ali.reza.buet@gmail.com>
2023-10-31 19:13:57 +0600

    Add method to update like counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,11 @@ class Like < ApplicationRecord
   belongs_to :post
 
   validates :user, uniqueness: { scope: :post, message: "You can only like a post once" }
+
+  after_create :update_likes_counter
+  after_destroy :update_likes_counter
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
