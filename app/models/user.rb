72e9ad9608e531ecc66f98b0e4131d575bafa6db
AliReza <ali.reza.buet@gmail.com>
2023-11-01 01:39:20 +0600

    Add validation for user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true, length: { maximum: 255 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :photo, presence: true, format: { with: %r{\Ahttps?://.+\z}, message: 'must be a valid URL' }
-  validates :bio, length: { maximum: 1000 }
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
