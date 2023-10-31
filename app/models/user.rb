8bf72b9182ea58a2aa4113227265455d0d2ec2c8
AliReza <ali.reza.buet@gmail.com>
2023-10-31 18:40:52 +0600

    Add method to user for displaying three posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 255 }
     validates :photo, presence: true, format: { with: /\Ahttps?:\/\/.+\z/, message: 'must be a valid URL' }
     validates :bio, length: { maximum: 1000 }
+
+    def recent_posts(limit = 3)
+        posts.order(created_at: :desc).limit(limit)
+    end
 end
