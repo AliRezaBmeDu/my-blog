9fbf71e7daa34510bd64d992583e096a00ac7cc4
AliReza <ali.reza.buet@gmail.com>
2023-11-01 11:38:07 +0600

    Regenerate the database

--- db/migrate/20231031204633_create_posts.rb
+++ db/migrate/20231031204633_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
+      t.references :user, foreign_key: { to_table: :users, column: :author_id }
       t.string :title
       t.text :text
       t.integer :comments_counter
@@ -9,6 +10,5 @@ class CreatePosts < ActiveRecord::Migration[7.1]
       t.timestamps
     end
 
-    add_index :posts, :author_id
   end
 end
