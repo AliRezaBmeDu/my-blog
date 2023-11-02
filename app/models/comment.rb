60aef1beddeeb6bde3f1a805bfb037659c011705
AliReza <ali.reza.buet@gmail.com>
2023-11-02 17:24:08 +0600

    Complete the test for post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  # belongs_to :user
+  belongs_to :post, class_name: 'Post'
 
   after_save :update_comments_counter
   after_destroy :update_comments_counter
