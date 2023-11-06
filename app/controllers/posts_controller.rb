29f09619c34f44d1e1788e27263795ec760a530b
AliReza <ali.reza.buet@gmail.com>
2023-11-07 00:03:57 +0600

    Update the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show]
+
+    # GET /posts
+    def index
+      @posts = Post.all
+    end
+  
+    # GET /posts/:id
+    def show
+        puts params
+    end
+
+    private
+
+    def set_post
+        puts "Find the post by id"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
   get 'users/index'
   get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -11,5 +13,5 @@ Rails.application.routes.draw do
   end
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index"
 end
