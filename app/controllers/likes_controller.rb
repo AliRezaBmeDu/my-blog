86efd6d31d059abba961c8afd93e655e7faf3f53
AliReza <ali.reza.buet@gmail.com>
2023-11-09 18:58:27 +0600

    Fix the like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,14 @@ class LikesController < ApplicationController
     def create
       @user = current_user
       @post = Post.find(params[:post_id])
-      @like = @post.likes.new(author: @user)
+      @like = @post.likes.new(user: @user)
   
       if @like.save
         redirect_to user_post_path(user_id: @post.author, id: @post), notice: 'Liked!'
       else
-        # Handle error
+        redirect_to user_post_path(user_id: @post.author, id: @post), notice: 'Like failed!'
       end
+
     end
   end
   
