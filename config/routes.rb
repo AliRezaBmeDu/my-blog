db3b56880f1c1877f78f01f514239fac0853c46d
AliReza <ali.reza.buet@gmail.com>
2023-11-16 03:17:54 +0600

    Readjust the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth'
+  
   get '/users', to: 'users#index', as: :users
   get '/users/:id', to: 'users#show', as: :user
   get '/users/:id/posts', to: 'posts#index', as: :user_posts
   get '/users/:user_id/posts/new', to: 'posts#new', as: :new_user_post
   get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
-  
 
-  resources :users do
+  resources :users, only: [:index, :show, :new] do
     resources :posts do
       member do
         post 'like', to: 'likes#create'
@@ -15,7 +15,16 @@ Rails.application.routes.draw do
       resources :comments
     end
   end
+
   resources :likes, only: [:create]
 
-  root "users#index"
+  authenticated :user do
+    root to: 'users#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new", as: :sign_in_root
+  end
+
+  # root 'users#index'
 end
