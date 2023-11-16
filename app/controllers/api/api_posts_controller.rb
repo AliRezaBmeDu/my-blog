b4d644d5b2934b4b3e620de67a71b7ba25b12e38
AliReza <ali.reza.buet@gmail.com>
2023-11-16 22:03:29 +0600

    add api posts controller

---
+++ app/controllers/api/api_posts_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    class ApiPostsController < ApplicationController
+      before_action :authenticate_user!
+      
+      def index
+        @user = current_user
+        @posts = @user.posts
+        render json: @posts
+      end
+    end
+  end
